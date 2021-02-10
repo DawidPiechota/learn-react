278b62c48ed835c2d411c4b367285a6ebd6040c5
DawidPiechota <dpiechotaa@gmail.com>
2021-02-11 00:05:02 +0100

    implement fake backend - json server

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -1,18 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import Bloglist from './Bloglist';
+
 const Home = () => {
 
-    const handleClick = (e) => {
-        console.log(e.target);
-    }
-    const handleClick2 = (name) => {
-        console.log("hi " + name);
-    }
+const blogsEndpoint = "http://192.168.1.37:8000/blogs";
+const [blogs, setBlogs] = useState(null);
+const [isPending, setIsPending] = useState(true);
+const [errorMessage, setErrorMessage] = useState(null);
 
+useEffect(() => {
+  async function fetchBlogs(endpoint) {
+    const response =  await fetch(endpoint);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const jsonData = await response.json();
+    setBlogs(jsonData);
+    setIsPending(false);
+    setErrorMessage(null);
+  }
+  setTimeout(() => {
+    fetchBlogs(blogsEndpoint).catch( e => {
+      setErrorMessage("We are experiencing technical difficulties. Please try again in a few minutes");
+      setIsPending(false);
+      console.log('There has been a problem with fetchBlogs(): ' + e.message);
+    });
+  },1000) // Fake request delay
+}, []);
 
     return ( 
-        <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click Me</button>
-            <button onClick={() => handleClick2("Dawid")}>Click me2</button>
+        <div className='home'>
+            { errorMessage && <div>{errorMessage}</div> }
+            { isPending && <div>Loading...</div> }
+            { blogs && <Bloglist blogs={blogs} title='All Blogs' />}
         </div>
      );
 }
