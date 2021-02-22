b436b5205628e008687be352fcc7ff218f0de392
DawidPiechota <dpiechotaa@gmail.com>
2021-02-22 21:33:26 +0100

    bruteforce travelling salesman

--- d3-with-react/src/Panel.js
+++ d3-with-react/src/Panel.js
@@ -1,12 +1,121 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Visualise from './Visualise';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import { Typography } from '@material-ui/core';
 
 
 
 const Panel = () => {
-  const [locations, setLocations] = useState([ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]);
+  const [locations, setLocations] = useState([]);
+  const [path, setPath] = useState([]);
+  const [links, setLinks] = useState([]);
+  const [totalPathLength, setTotalPathLength] = useState(0);
+  const svgWidth = 1025;
+  const svgHeight = 630;
+
+  useEffect(() => {
+    randomLocations();
+    //shufflePath();
+  }, []);
+
+  function randomLocations()  {
+    setPath([]);
+    setLinks([]);
+    let times = 10;
+    let newLocations = [];
+    for(let i = 0; i < times; i++) {
+      newLocations.push({x: getRandomInt(30, svgWidth-30), y: getRandomInt(30, svgHeight-30), id: i});
+    }
+    setLocations(newLocations);
+  }
+  
+  function getBetterPath() {
+
+    const pointDistance = (x1, y1, x2, y2) => {
+      var a = x1 - x2;
+      var b = y1 - y2;
+      return Math.sqrt( a*a + b*b );
+    }
+  
+    function pathLength(path) {
+      let sum = 0;
+      for(let i = 0; i < path.length; i++) {
+        sum+= pointDistance(
+          locations[path[i]].x,
+          locations[path[i]].y,
+          locations[path[(i + 1) % path.length]].x,
+          locations[path[(i + 1) % path.length]].y
+        );
+      }
+      return sum;
+    }
+
+    let bestCost = pathLength(path);
+    let currentCost = bestCost;
+    let currentPath = path.slice();
+    let bestPath = path.slice();
+    for(let i = 0; i < 20000; i ++) {
+      currentPath = shuffle(currentPath);
+      currentCost = pathLength(currentPath);
+      if(currentCost < bestCost) {
+        console.log(i);
+        bestPath = currentPath.slice();
+        bestCost = currentCost;
+      }
+    }
+    console.log("run");
+    setPath(bestPath);
+    setTotalPathLength(Math.floor(bestCost));
+    //setTimeout(getBetterPath, 1000);
+  }
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  function shuffle(array) {
+    var m = array.length, t, i;
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+    return array;
+  }
+
+  function shufflePath() {
+    let newPath = locations.map( l => l.id);
+    setPath(shuffle(newPath));
+  }
+
+  useEffect(() => {
+    function createLinks() {
+      let newLinks = [];
+      for(let i = 0; i < path.length; i++) {
+        let link = {
+          id: i,
+          from: path[i],
+          to: path[(i + 1) % path.length],
+          source: {
+            x: locations[path[i]].x,
+            y: locations[path[i]].y,
+          },
+          target: {
+            x: locations[path[(i + 1) % path.length]].x,
+            y: locations[path[(i + 1) % path.length]].y,
+          }
+        }
+        newLinks.push(link);
+      }
+      return newLinks;
+    }
+    
+    setLinks(createLinks());
+  }, [path]);
 
   return (
       <Grid
@@ -16,17 +125,27 @@ const Panel = () => {
         justify="center"
       >
         <Grid item xs={12}>
-          <Visualise locations={locations}/>
+          <Visualise locations={locations} links={links} svgWidth={svgWidth} svgHeight={svgHeight}/>
         </Grid>
         <Grid item xs={3}>
-          <Button variant="contained" color="primary" onClick={() => setLocations(locations.filter(value => value < 35))}>
-            Filter data
+          <Button variant="contained" color="primary" onClick={shufflePath}>
+            Draw path
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button variant="contained" color="primary" onClick={() => setLocations(locations.map(value => value + 5))}>
-            Update data
+          <Button variant="contained" color="primary" onClick={randomLocations}>
+            Random locations
           </Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button variant="contained" color="primary" onClick={getBetterPath}>
+            Solve
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            {totalPathLength}
+          </Typography>
         </Grid>
       </Grid>
    );
--- d3-with-react/src/Visualise.js
+++ d3-with-react/src/Visualise.js
@@ -1,25 +1,77 @@
 import { useRef, useEffect, useState } from "react";
 import { select } from "d3";
 
-const Visualise = ({locations}) => {
+const Visualise = ({locations, svgWidth, svgHeight, links}) => {
   const svgRef = useRef();
 
-  useEffect(() => {
+  function updateLinks() {
+    const svg = select(svgRef.current);
+    const link = d => {
+      return (
+        "M" +
+        d.source.x +
+        "," +
+        d.source.y +
+        " L" +
+        d.target.x +
+        "," +
+        d.target.y
+      );
+    };
+
+    const t = svg.transition()
+        .duration(750);
+
+    if(!links) {
+      svg
+      .selectAll("path")
+      .remove();
+    }
+
+    svg
+      .selectAll("path")
+      .data(links)
+      .join(
+        enter => enter.append("path")
+          .attr("d", link)
+          .attr("stroke", "black")
+          .attr("stroke-width", 2),
+      update => update
+          .attr("stroke-width", 2)
+        .call(update => update.transition(t)
+          .attr("d", link)),
+      exit => exit
+          .remove()
+      );
+      
+    svg.selectAll("circle").remove();
+    updateNodes();
+  }
+
+  function updateNodes() {
     const svg = select(svgRef.current);
     svg
       .selectAll("circle")
       .data(locations)
       .join("circle")
-      .attr("r", 10)
-      .attr("cx", value => value * 2)
-      .attr("cy", value => value * 2)
+      .attr("r", 5)
+      .attr("cx", value => value.x)
+      .attr("cy", value => value.y)
       .attr("stroke", "black")
       .attr("fill", "blue");
+  }
+
+  useEffect(() => {
+    updateLinks();
+  },[links])
+
+  useEffect(() => {
+    updateNodes();
   }, [locations]);
 
   return (
     <>
-      <svg style={{outline: 'thin solid black', width: "60vw", height: "60vh"}}ref={svgRef}></svg>
+      <svg style={{outline: 'thin solid black'}} ref={svgRef} height={svgHeight} width={svgWidth}></svg>
     </>
   );
 }
