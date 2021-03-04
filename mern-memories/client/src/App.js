86081ff8d1c4de1fe9202d82730132a5eedc727c
DawidPiechota <dpiechotaa@gmail.com>
2021-03-04 20:19:04 +0100

    add the logic and components to update specific post

--- mern-memories/client/src/App.js
+++ mern-memories/client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
@@ -9,6 +9,7 @@ import memories from './images/memories.png';
 import useStyles from './styles';
 
 const App = () => {
+  const [currentId, setCurrentId] = useState(null);
   const classes = useStyles();
   const dispatch = useDispatch();
 
@@ -25,11 +26,11 @@ const App = () => {
       <Grow in>
         <Container>
           <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts />
+            <Grid item xs={12} md={7}>
+              <Posts setCurrentId={setCurrentId} />
             </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form />
+            <Grid item xs={12} md={4}>
+              <Form setCurrentId={setCurrentId} currentId={currentId} />
             </Grid>
           </Grid>
         </Container>
--- mern-memories/client/src/components/Form/Form.js
+++ mern-memories/client/src/components/Form/Form.js
@@ -1,30 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import useStyles from './styles';
-import { createPost } from '../../actions/posts';
-const Form = () => {
-  const [postData, setPostData] = useState({
-    creator: '', title: '', message: '', tags: '', selectedFile: '',
-  });
+import { createPost, updatePost } from '../../actions/posts';
+
+const Form = ({currentId, setCurrentId}) => {
+  const defaultPostData = {creator: '', title: '', message: '', tags: '', selectedFile: '',}
+  const [postData, setPostData] = useState({...defaultPostData});
+  const postToUpdate = useSelector((state) => currentId ? state.posts.find(p => p._id === currentId) : null);
   const classes = useStyles();
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if(postToUpdate) setPostData(postToUpdate);
+  }, [postToUpdate]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    dispatch(createPost(postData));
+    if(currentId) {
+      dispatch(updatePost(currentId, postData));
+    } else {
+      dispatch(createPost(postData));
+    }
+    clear();
   }
 
   const clear = () => {
-
+    setCurrentId(null);
+    setPostData({...defaultPostData});
   }
+
   return ( 
     <Paper className={classes.paper}>
       <form className={`${classes.root} ${classes.form}`} autoComplete="off" noValidate onSubmit={handleSubmit}>
-        <Typography variant="h6">Creating a Memory</Typography>
+        <Typography variant="h6">{currentId ? 'Editing' : 'Creating'} a Memory</Typography>
         <TextField 
           name="creator" 
           variant="outlined" 
@@ -77,7 +89,7 @@ const Form = () => {
             variant="contained"
             color="secondary"
             size="small"
-            onClick={() => clear}
+            onClick={clear}
             fullWidth
           >Clear</Button>
       </form>
