f83f3fb1b5fb3efb5134f98361e2d6b6d98bcb09
DawidPiechota <dpiechotaa@gmail.com>
2021-03-05 02:13:27 +0100

    fix mobile responsiveness using materialUI breakpoints

--- mern-memories/client/src/App.js
+++ mern-memories/client/src/App.js
@@ -25,11 +25,11 @@ const App = () => {
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} md={7}>
+          <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item sm={12} md={7}>
               <Posts setCurrentId={setCurrentId} />
             </Grid>
-            <Grid item xs={12} md={4}>
+            <Grid item sm={12} md={4}>
               <Form setCurrentId={setCurrentId} currentId={currentId} />
             </Grid>
           </Grid>
