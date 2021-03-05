dca93ff8450a8b9b61d1e9e74b185176f209e36f
DawidPiechota <dpiechotaa@gmail.com>
2021-03-05 01:26:34 +0100

    add delete and like functionality

--- mern-memories/client/src/components/Posts/Post/Post.js
+++ mern-memories/client/src/components/Posts/Post/Post.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import EditIcon from '@material-ui/icons/Edit';
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
 
 import useStyles from './styles';
+import { deletePost, likePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
   return ( 
     <Card className={classes.card}>
       <CardMedia
@@ -25,23 +29,23 @@ const Post = ({ post, setCurrentId }) => {
           style={{color: 'white'}}
           size="small"
           onClick={() => setCurrentId(post._id)}
-        ><MoreHorizIcon fontSize="default"/>
+        ><EditIcon fontSize="default"/>
         </Button>
       </div>
       <div className={classes.details}>
-      <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
+      <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag}`)}</Typography>
       </div>
       <Typography className={classes.title} variant="h5" gutterBottom >{post.title}</Typography>
       <CardContent>
-        <Typography className={classes.title} variant="body2" gutterBottom >{post.message}</Typography>
+        <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
           <ThumbUpAltIcon fontSize="small"/>
           Like
           {post.likeCount}
         </Button>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fontSize="small"/>
           Delete
         </Button>
