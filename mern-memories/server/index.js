9fd2362620f58fa575b652bb8eda932ae87da7f1
DawidPiechota <dpiechotaa@gmail.com>
2021-03-09 16:09:55 +0100

    add env variables

--- mern-memories/server/index.js
+++ mern-memories/server/index.js
@@ -1,15 +1,13 @@
-//import 'dotenv/config.js';
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotnenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
-//config()
 
-
+dotnenv.config();
 const app = express();
-
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -17,7 +15,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://@cluster0.du8v5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
